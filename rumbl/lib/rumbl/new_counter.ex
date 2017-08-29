0850dee39407b93ed7df7d63e81e607d843a057a
woonkeekim1 <woonkeekim@gmail.com>
2017-08-28 23:11:22 -0700

    OTP

---
+++ rumbl/lib/rumbl/new_counter.ex
@@ -0,0 +1,27 @@
+defmodule Rumbl.NewCounter do
+  use GenServer
+
+  def inc(pid), do: GenServer.cast(pid, :inc)
+
+  def dec(pid), do: GenServer.cast(pid, :dec)
+
+  def val(pid) do
+    GenServer.call(pid, :val)
+  end
+
+  def start_link(init_val) do
+    GenServer.start_link(__MODULE__, init_val)
+  end
+
+  def handle_cast(:inc, val) do
+    {:noreply, val + 1}
+  end
+
+  def handle_cast(:dec, val) do
+    {:noreply, val - 1}
+  end
+
+  def handle_call(:val, _from, val) do
+    {:reply, val, val}
+  end
+end
--- rumbl/mix.exs
+++ rumbl/mix.exs
@@ -38,6 +38,7 @@ defmodule Rumbl.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
+     {:sweet_xml, "~> 0.5.0"},
      {:comeonin, "~> 2.0"}]
   end
 
